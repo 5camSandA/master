6206ca91d37e7a77a960fd582d88c9171f030a9d
neupane11 <sandip.neupane11@gmail.com>
2021-06-25 16:16:00 -0600

    files edited

--- flirsave.py
+++ flirsave.py
@@ -3,15 +3,15 @@ from skimage.io import imsave
 import numpy as np
 
 camera = Lepton()
-image = camera.grab().astype(np.float32)
+#image = camera.grab().astype(np.float32)
 image1 = camera.grab().astype(np.float32)
-image2 = camera.grab().astype(np.uint32)
-image3 = camera.grab().astype(np.uint32)
+#image2 = camera.grab().astype(np.uint32)
+#image3 = camera.grab().astype(np.uint32)
 
-print(image[0][0], image[0][0].astype(np.uint8))
+'''print(image[0][0], image[0][0].astype(np.uint8))
 print(image1[0][0], image1[0][0].astype(np.uint8))
 print(image2[0][0], image2[0][0].astype(np.uint8))
-print(image3[0][0], image3[0][0].astype(np.uint8))
+print(image3[0][0], image3[0][0].astype(np.uint8))'''
 
 #print("major version:", camera.major_version)
 #print("minor version:", camera.minor_version)
@@ -34,21 +34,21 @@ camera.close()
 #print(image.max())
 
 #img = 255*(<value-min_value>/<value_range>)
-image = 200*(image-image.min())/(image.max()-image.min())
-image1 = 400*(image1-image1.min())/(image1.max()-image1.min())
-image2 = 200*(image2-image2.min())/(image2.max()-image2.min()) 
-image3 = 400*(image3-image3.min())/(image3.max()-image3.min())
-print(image[0][0], image.min(), image.max())
+#image = 200*(image-image.min())/(image.max()-image.min())
+image1 = 256*(image1-image1.min())/(image1.max()-image1.min())
+#image2 = 200*(image2-image2.min())/(image2.max()-image2.min()) 
+#image3 = 400*(image3-image3.min())/(image3.max()-image3.min())
+'''print(image[0][0], image.min(), image.max())
 print(image1[0][0], image1.min(), image1.max())
 print(image2[0][0], image2.min(), image2.max())
-print(image3[0][0], image3.min(), image3.max())
+print(image3[0][0], image3.min(), image3.max())'''
 
 #print((image-image.min())/(image.max()-image.min()))
 
-imsave("irtest.jpg", image.astype(np.uint8))
+#imsave("irtest.jpg", image.astype(np.uint8))
 imsave("irtest1.jpg", image1.astype(np.uint8))
-imsave("irtest2.jpg", image2.astype(np.uint8))
-imsave("irtest3.jpg", image3.astype(np.uint8))
+#imsave("irtest2.jpg", image2.astype(np.uint8))
+#imsave("irtest3.jpg", image3.astype(np.uint8))
 
 camera.close()
 
